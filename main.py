ec5fe0a7dd2781c9c24758e28fda57c51aa03b8e
GEORGIOS MOYSIADIS <145147138+georgemois23@users.noreply.github.com>
2025-06-12 18:32:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -90,6 +90,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",  # Keep localhost for local development
         "https://pamac.moysiadis.codes"  # Add your deployed frontend URL
+        "https://pamac.moysiadis.dev/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
